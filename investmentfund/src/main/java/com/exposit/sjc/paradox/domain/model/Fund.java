829af42845cc2b4db066828d5791f6e1c31880d5
xxstelmah <stelmah2121@mail.ru>
2015-08-12 23:07:33 +0300

    update model

--- investmentfund/src/main/java/com/exposit/sjc/paradox/domain/model/Fund.java
+++ investmentfund/src/main/java/com/exposit/sjc/paradox/domain/model/Fund.java
@@ -1,5 +1,53 @@
 package com.exposit.sjc.paradox.domain.model;
 
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "fund")
 public class Fund {
 
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "fund_id")
+	private int fundId;
+	
+	@OneToOne
+	@JoinColumn(name = "fund_fundAccount")
+	private Account fundAccount;
+	
+	@OneToMany
+	@JoinColumn(name = "fund_fundsAreas")
+	private List<FundArea> fundsAreas;
+ 
+	public int getFundId() {
+		return fundId;
+	}
+	
+	public void setFundId(int fundId) {
+		this.fundId = fundId;
+	}
+	
+	public Account getFundAccount() {
+		return fundAccount;
+	}
+	
+	public void setFundAccount(Account fundAccount) {
+		this.fundAccount = fundAccount;
+	}
+	
+	public List<FundArea> getFundsAreas() {
+		return fundsAreas;
+	}
+	
+	public void setFundsAreas(List<FundArea> fundsAreas) {
+		this.fundsAreas = fundsAreas;
+	}
 }
--- investmentfund/src/main/java/com/exposit/sjc/paradox/domain/model/FundArea.java
+++ investmentfund/src/main/java/com/exposit/sjc/paradox/domain/model/FundArea.java
@@ -1,5 +1,53 @@
 package com.exposit.sjc.paradox.domain.model;
 
-public class FundArea {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "fundArea")
+public class FundArea {
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "fundArea_fundAreaId")
+	private int fundAreaId;
+	
+	@OneToOne
+	@JoinColumn( name = "fundArea_fundAreaAccount")
+	private Account fundAreaAccount;
+	
+	@Column(name = "fundArea_currentArea")
+	@Enumerated(EnumType.STRING)
+	private Area currentArea;
+	
+	public int getFundAreaId() {
+		return fundAreaId;
+	}
+	
+	public void setFundAreaId(int fundAreaId) {
+		this.fundAreaId = fundAreaId;
+	}
+	
+	public Account getFundAreaAccount() {
+		return fundAreaAccount;
+	}
+	
+	public void setFundAreaAccount(Account fundAreaAccount) {
+		this.fundAreaAccount = fundAreaAccount;
+	}
+	
+	public Area getCurrentArea() {
+		return currentArea;
+	}
+	
+	public void setCurrentArea(Area currentArea) {
+		this.currentArea = currentArea;
+	}
+	
+	
 }
